3da97284b886ab2c620395006f3856022f312e96
jeeneo <187322222+jeeneo@users.noreply.github.com>
2025-10-29 16:52:37 -0400

    version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.je.fontsmanager.samsung"
         minSdk = 36
         targetSdk = 36
-        versionCode = 110
-        versionName = "1.1"
+        versionCode = 120
+        versionName = "1.2"
     }
     buildTypes {
         release {
