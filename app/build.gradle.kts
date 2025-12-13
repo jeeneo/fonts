2a527b3b0bf81c4daedc68b26d7ca2145bebada5
jeeneo <187322222+jeeneo@users.noreply.github.com>
2025-12-12 23:28:58 -0500

    version bump
    always forget this, should automate i guess

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.je.fontsmanager.samsung"
         minSdk = 36
         targetSdk = 36
-        versionCode = 140
-        versionName = "1.4"
+        versionCode = 150
+        versionName = "1.5"
     }
     buildTypes {
         release {
